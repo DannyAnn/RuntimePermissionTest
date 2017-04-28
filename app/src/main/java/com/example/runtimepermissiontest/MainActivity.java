2fc2bcb7ea766ce61eae2d9dc7c606b6949d021a
Danny <dannyann95@foxmail.com>
2017-04-28 10:08:10 +0800

    新增加的代码就是Android7.0系统中我们提供的运行时权限的使用法，当然必须装有Android7.0系统的手机上才会看到效果。

--- app/src/main/java/com/example/runtimepermissiontest/MainActivity.java
+++ app/src/main/java/com/example/runtimepermissiontest/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.runtimepermissiontest;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.security.Security;
@@ -18,18 +25,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //在Android低于6.0的系统中，Google还没有引入运行时权限，此时只要注册文件申请程序所需要的权限即可。
-        Button button = (Button)findViewById(R.id.button);
+//        Button button = (Button)findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                try{
+//                    Intent intent = new Intent(Intent.ACTION_CALL);
+//                    intent.setData(Uri.parse("tel:10086"));
+//                    startActivity(intent);
+//                }catch (SecurityException e){
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+
+        //在Android7.0中我们引入了运行时权限，具体使用方法如下
+        Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try{
-                    Intent intent = new Intent(Intent.ACTION_CALL);
-                    intent.setData(Uri.parse("tel:10086"));
-                    startActivity(intent);
-                }catch (SecurityException e){
-                    e.printStackTrace();
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+                } else {
+                    call();
                 }
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if(grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    call();
+                }else {
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
+    }
+
+    private void call(){
+        try{
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel:10086"));
+            startActivity(intent);
+        }catch (SecurityException e){
+            e.printStackTrace();
+        }
     }
 }
